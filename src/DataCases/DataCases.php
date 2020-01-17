39011f4e95146ce19dca70c39a9701f84fa6e928
Gregs <gregdmat@gmail.com>
2020-01-16 23:11:41 -0300

    initial commit

---
+++ src/DataCases/DataCases.php
@@ -0,0 +1,20 @@
+<?php
+
+
+namespace Gregdmat\LaravelTestUtilities\DataCases;
+
+
+use Illuminate\Database\Eloquent\Model;
+
+class DataCases
+{
+
+    public function generate(array $stucture, Model $model)
+    {
+
+        foreach ($stucture as $item)
+        {
+
+        }
+    }
+}
